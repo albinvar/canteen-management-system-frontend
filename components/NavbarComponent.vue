593bdcca1b6954c8b9d361bce574417c757f0e18
Albin Varghese <albin8285@gmail.com>
2022-05-20 12:20:04 +0530

    chore: fix sm view navbar and remove nuxt default views

--- components/NavbarComponent.vue
+++ components/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- navbar -->
-  <div x-data="{ open: false }" class="w-full text-gray-700 bg-cream">
+  <div class="w-full text-gray-700 bg-cream">
     <div class="flex flex-col max-w-screen-xl px-8 mx-auto md:items-center md:justify-between md:flex-row">
       <div class="flex flex-row items-center justify-between py-6">
         <div class="relative md:mt-8">
@@ -11,8 +11,8 @@
         </div>
         <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
           <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
-            <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-            <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            <path v-if="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+            <path v-if="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
           </svg>
         </button>
       </div>
@@ -30,7 +30,13 @@
 
 <script>
 export default {
-  name: 'NavbarComponent.vue'
+  name: 'NavbarComponent',
+
+  data () {
+    return {
+      open: false
+    }
+  }
 }
 </script>
 
